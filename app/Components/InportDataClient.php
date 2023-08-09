4b13343349e88565594e04209b30ba0ce46c7211
Igor_chu <igorek_churilow@mail.ru>
2023-08-09 21:41:47 +0400

    work with guzzle

---
+++ app/Components/InportDataClient.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Components;
+
+
+use GuzzleHttp\Client;
+
+class InportDataClient
+{
+
+    public $client;
+
+    /**
+     * InportDataClient constructor.
+     * @param $client
+     */
+    public function __construct()
+    {
+
+        $this->client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => 'https://jsonplaceholder.typicode.com/',
+            // You can set any number of default request options.
+            'timeout'  => 2.0,
+            'verify' => false,
+        ]);
+
+    }
+
+}
